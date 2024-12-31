0d790dcc1214278f6f65718c82d1bc6bb07556a3
21127698 <lunnhok03@gmail.com>
2024-12-30 17:48:53 -0800

    day code len

---
+++ web.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "<h1>Xin chào!</h1>"
+
+@app.route("/gioi-thieu")
+def gioi_thieu():
+  return "<h2>Đây là trang giới thiệu</h2>"
+
+if __name__ == "__main__":
+  app.run(debug=True)
